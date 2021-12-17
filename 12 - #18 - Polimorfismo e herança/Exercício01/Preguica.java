cc880575dfcf80ca180b6e5fcb371ec2e1e87130
Kaylaine Fragoso <95701653+KaylaineFragoso@users.noreply.github.com>
2021-12-16 23:48:51 -0300

    Add files via upload

---
+++ 12 - #18 - Polimorfismo e herança/Exercício01/Preguica.java
@@ -0,0 +1,15 @@
+package PolimorfismoHerança;
+
+		public class Preguica extends Animal{
+	
+	private String subir;
+	
+	public String getSubir(){
+	return subir;
+	}
+
+	public void setSubir(String subir){
+	this.subir = subir;
+	}
+
+}
